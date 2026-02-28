b5efb1f4e045a960162a00bf872e716601018df9
Rachel <rachelngocxle@gmail.com>
2026-02-28 14:23:36 -0700

    failed test for testCountCities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -7,9 +7,27 @@ public class CustomListTest {
     public void testHasCity() {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+
         list.addCity(calgary);
+
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
+
+        assertFalse(list.hasCity(edmonton));
+    }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+
+        assertEquals(0, list.countCities());
+
+        list.addCity(new City("Calgary", "AB"));
+        assertEquals(1, list.countCities());
+
+        list.addCity(new City("Edmonton", "AB"));
+        assertEquals(2, list.countCities());
     }
 
 }
